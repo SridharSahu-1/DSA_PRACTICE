5bede50e1447b0ab21ec2fda752503e6952eadc6
Sridhar Sahu <sridharsahu5555@gmail.com>
2022-11-14 20:20:56 +0530

    Trees

---
+++ src/Recursion/RemoveLinkedlistElement.java
@@ -0,0 +1,19 @@
+package Recursion;
+ class ListNode {
+     int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+ class RemoveLinkedlistElement {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head==null) return null;
+        head.next= removeElements(head.next,val);
+        if(head.val==val){
+            return head=head.next;
+        }else{
+            return head=head;
+        }
+    }
+}
