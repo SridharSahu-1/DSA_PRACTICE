fa34f3df7364b16ce8ba8f51d212b9ff913d6188
Sridhar Sahu <sridharsahu5555@gmail.com>
2022-12-06 23:19:40 +0530

    DP Question

---
+++ src/Recursion/Permutation.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class Permutation {
+    public static void main(String[] args) {
+//        1 3 6 2 5 4 3 2 4
+        int[] arr = {1,3,6,2,5,4,3,2,4};
+        int target=9;
+        System.out.println(countSum(arr,target));
+    }
+
+    public static int countSum(int[] arr,int target){
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                if(arr[i]+arr[j]==target){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
