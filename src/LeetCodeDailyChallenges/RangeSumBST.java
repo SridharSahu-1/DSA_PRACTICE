2b605cb1f2a85c67439c4372d94032470ef61a8d
Sridhar Sahu <sridharsahu5555@gmail.com>
2022-12-08 21:24:24 +0530

    DP Question

--- src/LeetCodeDailyChallenges/RangeSumBST.java
+++ src/LeetCodeDailyChallenges/RangeSumBST.java
@@ -3,11 +3,11 @@ package LeetCodeDailyChallenges;
 //  Definition for a binary tree node.
 class TreeNode {
       int val;
-      TreeNode left;
-      TreeNode right;
+      TreeNode1 left;
+      TreeNode1 right;
       TreeNode() {}
       TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
+      TreeNode(int val, TreeNode1 left, TreeNode1 right) {
           this.val = val;
           this.left = left;
           this.right = right;
@@ -15,7 +15,7 @@ class TreeNode {
   }
 
 class RangeSumBST {
-    public int rangeSumBST(TreeNode root, int low, int high) {
+    public int rangeSumBST(TreeNode1 root, int low, int high) {
         if(root==null) return 0;
 
         if(root.val>=low && root.val<=high){
