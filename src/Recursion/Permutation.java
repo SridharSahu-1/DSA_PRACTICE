c4da0c40825d5b361ec3403d8d64b905e5b9fd71
Sridhar Sahu <sridharsahu5555@gmail.com>
2022-12-15 22:46:32 +0530

    DP Question

--- src/Recursion/Permutation.java
+++ src/Recursion/Permutation.java
@@ -4,6 +4,7 @@ public class Permutation {
     public static void main(String[] args) {
 //        1 3 6 2 5 4 3 2 4
         int[] arr = {1,3,6,2,5,4,3,2,4};
+        int[] arr1 ={1,23,1,321,324,321,1};
         int target=9;
         System.out.println(countSum(arr,target));
     }
